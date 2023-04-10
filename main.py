8b005d6e8bfcff93fb0099907d76da7610b43619
ArtroxGabriel <artroxgabriel@gmail.com>
2023-04-10 19:10:54 -0300

    main completo

--- main.py
+++ main.py
@@ -1,16 +1,42 @@
+
+def DFS(ListaADJ, vertice, Visitados, Componentes):
+  Visitados.add(vertice)
+  Componentes.add(vertice)
+  for N in ListaADJ[vertice]:
+    if N not in Visitados:
+      DFS(ListaADJ, N, Visitados, Componentes)
+
+
+def ProcurarConexas(ListaADJ):
+    VerticesVisitados = set()
+    Compenentes = []
+    for Vertice in ListaADJ:
+        if Vertice not in VerticesVisitados:
+            component = set()
+            DFS(ListaADJ, Vertice, VerticesVisitados, component)
+            Compenentes.append(component)
+    return Compenentes
+
 trash = input()  # trash: variavel descartavel
 trash = input()
 trash, NumIndices = input().split("n=")
 NumIndices = int(NumIndices)
 trash = input()
-print(NumIndices)
 
 status = True
-ListaADJ = [[] for _ in range(NumIndices)]
-'''
-while status:
+ListaADJ = {}
+for aux in range(1, NumIndices+1):
+  ListaADJ[aux] = [aux]
+
+while status: #Pegar os valores de entrada
   try:
-    user_input = input("Enter some input: ")
+    aux1, aux2 = input().split(" ")
+    ListaADJ[int(aux1)].append(int(aux2))
+    ListaADJ[int(aux2)].append(int(aux1))
   except EOFError:
     status = False
-'''
+
+saida = ProcurarConexas(ListaADJ)  
+
+for aux in range(len(saida)): #SAIDA do programa
+  print(" ".join(map(str, sorted(saida[aux]))))
