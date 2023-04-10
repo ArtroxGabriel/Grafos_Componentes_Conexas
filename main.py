da4cb249cce2d290b7060767ad6c785cabfa4518
Replit user <>
2023-04-10 12:25:21 +0000

    Main e arquivo de exemplos

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 trash = input()  # trash: variavel descartavel
 trash = input()
-trash, NumIndices = input().split("n = ")
+trash, NumIndices = input().split("n=")
 NumIndices = int(NumIndices)
 trash = input()
 print(NumIndices)
 
 status = True
-
+ListaADJ = [[] for _ in range(NumIndices)]
+'''
 while status:
   try:
     user_input = input("Enter some input: ")
   except EOFError:
     status = False
+'''
